8d7f8564cf6a423c96a07584efdd30023a060e67
Andrew Fei <afei3@illinois.edu>
2020-10-23 01:29:15 -0500

    Created cursor class

---
+++ Cursor.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+
+using namespace std;
+
+class Cursor {
+	private:
+		int x;
+		int y;
+	public:
+};
---
+++ Map.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+
+using namespace std;
+
+class Map {
+	private:
+		string map_name;
+	public:
+		string getMapName();
+};
